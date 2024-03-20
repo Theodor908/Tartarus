df78daa5768694fa87dd18db8198df2f05613360
Theodor908 <vasile.theodor908@gmail.com>
2024-03-20 21:40:09 +0200

    Beginning of healthStat

--- Assets/Code/Scripts/Character/Player/Player UI/PlayerUIHUDManager.cs
+++ Assets/Code/Scripts/Character/Player/Player UI/PlayerUIHUDManager.cs
@@ -6,11 +6,22 @@ namespace Tartarus
 {
     public class PlayerUIHUDManager : MonoBehaviour
     {
+        [SerializeField] UI_StatusBar healthBar;
         [SerializeField] UI_StatusBar staminaBar;
+
+        public void setNewHealthValue(float value)
+        {
+            healthBar.SetStatus(Mathf.RoundToInt(value));
+        }
+
+        public void setMaxHealtValue(float value)
+        {
+            healthBar.SetMaxStatus(value);
+        }
 
         public void setNewStaminaValue(float value)
         {
-            staminaBar.SetStatus(value);
+            staminaBar.SetStatus(Mathf.RoundToInt(value));
         }
 
         public void setMaxStaminaValue(float value)
